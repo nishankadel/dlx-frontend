1d1df343ae3b0d76c7de8bd6e9f92cb4401b455e
nishankadel <nishankadel39@gmail.com>
2022-06-26 20:33:38 +0545

    product completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
 
 const Home = () => {
-
+  const data = JSON.parse(localStorage.getItem("persist:dlx"));
+  console.log(data);
   return (
     <>
       <HomeCarousel />
